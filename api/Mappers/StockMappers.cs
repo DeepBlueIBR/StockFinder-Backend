61a1425c19c255a365ca911b5378dd5568828510
DeepBlueIBR <romeosa@hotmail.gr>
2025-11-12 19:50:53 +0200

    Backend for StocksWebApp

---
+++ api/Mappers/StockMappers.cs
@@ -0,0 +1,35 @@
+using api.Dtos.Stock;
+using api.Models;
+
+namespace api.Mappers
+{
+    public static class StockMappers
+    {
+        public static StockDto ToStockDto(this Stock stock)
+        {
+            return new StockDto
+            {
+                Id = stock.Id,
+                Symbol = stock.Symbol,
+                CompanyName = stock.CompanyName,
+                Purchase = stock.Purchase,
+                LastDiv = stock.LastDiv,
+                Industry = stock.Industry,
+                MarketCap = stock.MarketCap
+            };
+        }
+
+        public static Stock ToStockFromCreateDTO(this CreateStockRequestDto stockDto)
+        {
+            return new Stock
+            {
+                Symbol = stockDto.Symbol,
+                CompanyName = stockDto.CompanyName,
+                Purchase = stockDto.Purchase,
+                LastDiv = stockDto.LastDiv, 
+                Industry = stockDto.Industry,
+                MarketCap = stockDto.MarketCap
+            };
+        }
+    }
+}
